c507995f15ebe81873d715b4165e4f899f5eaf60
David Torosyan <davidtorosyan.git@gmail.com>
2022-03-16 14:49:30 -0700

    Parse stats

--- src/madness.py
+++ src/madness.py
@@ -2,15 +2,14 @@
 
 from bracket import get_bracket
 from common import DEFAULT_YEAR
-from stats import get_stat_urls, get_stats, parse_stats
+from stats import get_stat_urls, get_stats_for_team
 
 def main():
     print('Hello, world!')
     year = DEFAULT_YEAR
     bracket = get_bracket(year)
     stat_urls = get_stat_urls(year, bracket.teams.values())
-    stats_path = get_stats(year, stat_urls.urls[0], bracket.teams[0].abbrev)
-    print(parse_stats(stats_path))
+    print(get_stats_for_team(year, bracket.teams[0], stat_urls))
 
 if __name__ == '__main__':
     main()
--- src/stats.py
+++ src/stats.py
@@ -15,7 +15,8 @@ from common import get_transform_typed, get_or_download_path
 STATS_HOME_URL = 'https://www.cbssports.com/college-basketball/teams/'
 STATS_HOME_FILENAME = 'stats_home.html'
 STATS_URLS_FILENAME = 'stats_urls.json'
-STATS_TEAM_FORMAT = 'teams/{}.html'
+STATS_TEAM_RAW_FORMAT = 'teams/{}.html'
+STATS_TEAM_FORMAT = 'teams/{}.json'
 
 NAME_OVERRIDES = {
     'UConn': 'Connecticut Huskies',
@@ -33,11 +34,85 @@ NAME_OVERRIDES = {
 class StatUrls(BaseModel):
     urls: Dict[int, str]
 
-def parse_stats(path: str):
-    return path
+class Player(BaseModel):
+    name: str
+    position: str
 
-def get_stats(year: int, url: str, abbrev: str, force=False) -> str:
-    filename = STATS_TEAM_FORMAT.format(abbrev)
+class HeaderColumn(BaseModel):
+    index: int
+    name: str
+    description: str
+
+class PlayerRow(BaseModel):
+    player: Player
+    values: List[str]
+
+class Table(BaseModel):
+    name: str
+    header: List[HeaderColumn]
+    rows: List[PlayerRow]
+
+class StatsPage(BaseModel):
+    name: str
+    url: str
+    tables: List[Table]
+
+def get_stats_for_team(
+        year, 
+        team: Team, 
+        urls: StatUrls,
+        force_transform=False, 
+        force_fetch=False,
+    ) -> Dict[int, str]:
+    filename = STATS_TEAM_FORMAT.format(team.abbrev)
+    url = urls.urls[team.index]
+    return get_transform_typed(
+        year=year, 
+        filename=filename,
+        raw_func=lambda y,force=False: get_stats_raw(y, url, team.abbrev, force),
+        transform_func=parse_stats,
+        load_func=StatsPage,
+        force_transform=force_transform,
+        force_fetch=force_fetch,
+    )
+
+def parse_stats(path: str):
+    with open(path) as file:
+        soup = BeautifulSoup(file, features='html.parser')
+    return StatsPage(
+        name = soup.find(class_='PageTitle-header').text.strip(),
+        url = soup.find(rel='canonical')['href'],
+        tables = [parse_table(t) for t in soup.find_all(class_='TableBase')],
+    )
+
+def parse_table(soup: BeautifulSoup) -> Table:
+    return Table(
+        name = soup.find(class_='TableBase-title').text.strip(),
+        header = [parse_header(h, idx) for idx, h in enumerate(soup.find_all(class_='TableBase-headTh--number'))],
+        rows = [parse_row(h) for h in soup.select('.TableBase-bodyTr:not(.TableBase-bodyTr--total)')],
+    )
+
+def parse_header(soup: BeautifulSoup, idx: int) -> HeaderColumn:
+    return HeaderColumn(
+        index = idx,
+        name = soup.a.text.strip(),
+        description = soup.div.text.strip(),
+    )
+
+def parse_row(soup: BeautifulSoup) -> PlayerRow:
+    return PlayerRow(
+        player = parse_player(soup.find(class_='CellPlayerName--long')),
+        values = [v.text.strip() for v in soup.find_all(class_='TableBase-bodyTd--number')],
+    )
+
+def parse_player(soup: BeautifulSoup) -> Player:
+    return Player(
+        name = soup.span.a.text.strip(),
+        position = soup.span.span.text.strip(),
+    )
+
+def get_stats_raw(year: int, url: str, abbrev: str, force=False) -> str:
+    filename = STATS_TEAM_RAW_FORMAT.format(abbrev)
     return get_or_download_path(year, url, filename, force)
 
 def get_stat_urls(
