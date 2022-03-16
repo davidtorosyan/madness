3f8139950e1f6610642653c3750b8d6b99e3e928
David Torosyan <davidtorosyan.git@gmail.com>
2022-03-15 19:53:07 -0700

    Compute next match index

--- src/bracket.py
+++ src/bracket.py
@@ -5,7 +5,7 @@ import os.path
 import requests
 
 from collections import namedtuple
-from typing import List
+from typing import List, Optional
 
 from bs4 import BeautifulSoup
 from pydantic import BaseModel
@@ -14,6 +14,17 @@ from common import data_dir, data_dir_assert
 
 BRACKET_URL_FORMAT = 'https://fantasy.espn.com/tournament-challenge-bracket/{}/en/bracket'
 BRACKET_RAW_FILENAME = 'bracket.html'
+EXPECTED_NUM_TEAMS = 64
+
+def matches_per_round():
+    num_teams = EXPECTED_NUM_TEAMS
+    rounds = []
+    while num_teams > 1:
+        num_teams = int(num_teams / 2)
+        rounds.append(num_teams)
+    return rounds
+
+EXPECTED_MATCHES_PER_ROUND = matches_per_round()
 
 class Team(BaseModel):
     name: str
@@ -21,9 +32,11 @@ class Team(BaseModel):
     seed: int
 
 class Match(BaseModel):
-    location: str
     index: int
+    location: str
     teams: List[Team]
+    teams_in_round: int
+    next_match_index: Optional[int]
 
 def parse_raw_bracket(year):
     path = get_raw_bracket_path(year)
@@ -36,10 +49,14 @@ def parse_raw_bracket(year):
 
 def parse_matchup(soup):
     teams = soup.find_all(class_ = 'actual')
+    index = int(soup['data-index'])
+    next_match_index, teams_in_round = compute_next_match_index(index)
     return Match(
         location = soup['data-location'], 
-        index = soup['data-index'],
-        teams = [parse_team(t) for t in teams]
+        index = index,
+        teams = [parse_team(t) for t in teams],
+        teams_in_round = teams_in_round,
+        next_match_index = next_match_index,
     )
 
 def parse_team(soup):
@@ -48,6 +65,25 @@ def parse_team(soup):
         abbrev = soup.find(class_= 'abbrev').text, 
         seed = soup.find(class_= 'seed').text,
     )
+
+def compute_next_match_index(index):
+    round = 0
+    num_matches_in_round = EXPECTED_MATCHES_PER_ROUND[round]
+    curr = index
+    total_matches = num_matches_in_round
+    while curr >= num_matches_in_round:
+        curr -= num_matches_in_round
+        round += 1
+        if round >= len(EXPECTED_MATCHES_PER_ROUND) - 1:
+            return (None, 1)
+        num_matches_in_round = EXPECTED_MATCHES_PER_ROUND[round]
+        total_matches += num_matches_in_round
+    ratio = curr / num_matches_in_round
+    matches_in_next_round = EXPECTED_MATCHES_PER_ROUND[round+1]
+    place_in_next_round = ratio * matches_in_next_round
+    result = total_matches + place_in_next_round
+    teams_in_round = num_matches_in_round * 2
+    return (int(result), teams_in_round)
 
 def get_raw_bracket_path(year, force = False):
     raw_bracket_path = os.path.join(data_dir(year), BRACKET_RAW_FILENAME)
