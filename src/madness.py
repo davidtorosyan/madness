82401b37b140af7f09196450ca5b4ad39e58918a
David Torosyan <davidtorosyan.git@gmail.com>
2022-03-16 18:21:51 -0700

    Parse roster

--- src/madness.py
+++ src/madness.py
@@ -4,13 +4,15 @@ from bracket import get_bracket
 from common import DEFAULT_YEAR
 from home import get_team_urls
 from stats import get_stats_for_team
+from roster import get_roster_for_team
 
 def main():
     print('Hello, world!')
     year = DEFAULT_YEAR
     bracket = get_bracket(year)
     stat_urls = get_team_urls(year, bracket.teams.values())
-    print(get_stats_for_team(year, bracket.teams[0], stat_urls))
+    # print(get_stats_for_team(year, bracket.teams[0], stat_urls))
+    print(get_roster_for_team(year, bracket.teams[0], stat_urls, force_transform=True))
 
 if __name__ == '__main__':
     main()
--- src/roster.py
+++ src/roster.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from bs4 import BeautifulSoup
 from home import TeamInfo
@@ -9,17 +9,19 @@ from pydantic import BaseModel
 from bracket import Team
 from common import get_transform_typed, get_or_download_path
 
-STATS_TEAM_RAW_FORMAT = 'stats/{}.html'
-STATS_TEAM_FORMAT = 'stats/{}.json'
+ROSTER_TEAM_RAW_FORMAT = 'roster/{}.html'
+ROSTER_TEAM_FORMAT = 'roster/{}.json'
+
+class Injury(BaseModel):
+    area: str
 
 class Player(BaseModel):
     name: str
-    position: str
+    injury: Optional[Injury]
 
 class HeaderColumn(BaseModel):
     index: int
     name: str
-    description: str
 
 class PlayerRow(BaseModel):
     player: Player
@@ -30,65 +32,69 @@ class Table(BaseModel):
     header: List[HeaderColumn]
     rows: List[PlayerRow]
 
-class StatsPage(BaseModel):
+class RosterPage(BaseModel):
     name: str
     url: str
-    tables: List[Table]
+    table: Table
 
-def get_stats_for_team(
+def get_roster_for_team(
         year, 
         team: Team, 
         info: TeamInfo,
         force_transform=False, 
         force_fetch=False,
     ) -> Dict[int, str]:
-    filename = STATS_TEAM_FORMAT.format(team.abbrev)
-    url = info.urls[team.index].stats
+    filename = ROSTER_TEAM_FORMAT.format(team.abbrev)
+    url = info.urls[team.index].roster
     return get_transform_typed(
         year=year, 
         filename=filename,
-        raw_func=lambda y,force=False: get_stats_raw(y, url, team.abbrev, force),
-        transform_func=parse_stats,
-        load_func=StatsPage,
+        raw_func=lambda y,force=False: get_roster_raw(y, url, team.abbrev, force),
+        transform_func=parse_roster,
+        load_func=RosterPage,
         force_transform=force_transform,
         force_fetch=force_fetch,
     )
 
-def parse_stats(path: str):
+def parse_roster(path: str):
     with open(path) as file:
         soup = BeautifulSoup(file, features='html.parser')
-    return StatsPage(
+    return RosterPage(
         name = soup.find(class_='PageTitle-header').text.strip(),
         url = soup.find(rel='canonical')['href'],
-        tables = [parse_table(t) for t in soup.find_all(class_='TableBase')],
+        table = parse_table(soup.find(class_='TableBase')),
     )
 
 def parse_table(soup: BeautifulSoup) -> Table:
+    headers = soup.find_all(class_='TableBase-headTh')
+    use_headers = headers[0:1] + headers[2:]
     return Table(
         name = soup.find(class_='TableBase-title').text.strip(),
-        header = [parse_header(h, idx) for idx, h in enumerate(soup.find_all(class_='TableBase-headTh--number'))],
-        rows = [parse_row(r) for r in soup.select('.TableBase-bodyTr:not(.TableBase-bodyTr--total)')],
+        header = [parse_header(h, idx) for idx, h in enumerate(use_headers)],
+        rows = [parse_row(r) for r in soup.find_all(class_='TableBase-bodyTr')],
     )
 
 def parse_header(soup: BeautifulSoup, idx: int) -> HeaderColumn:
     return HeaderColumn(
         index = idx,
-        name = soup.a.text.strip(),
-        description = soup.div.text.strip(),
+        name = soup.text.strip(),
     )
 
 def parse_row(soup: BeautifulSoup) -> PlayerRow:
+    values = soup.find_all(class_='TableBase-bodyTd')
+    use_values = values[0:1] + values[2:]
     return PlayerRow(
         player = parse_player(soup.find(class_='CellPlayerName--long')),
-        values = [v.text.strip() for v in soup.find_all(class_='TableBase-bodyTd--number')],
+        values = [v.text.strip() for v in use_values],
     )
 
 def parse_player(soup: BeautifulSoup) -> Player:
+    tooltip = soup.find(class_='Tablebase-tooltipInner')
     return Player(
         name = soup.span.a.text.strip(),
-        position = soup.span.span.text.strip(),
+        injury = Injury(area=tooltip.text.strip()) if tooltip else None,
     )
 
-def get_stats_raw(year: int, url: str, abbrev: str, force=False) -> str:
-    filename = STATS_TEAM_RAW_FORMAT.format(abbrev)
+def get_roster_raw(year: int, url: str, abbrev: str, force=False) -> str:
+    filename = ROSTER_TEAM_RAW_FORMAT.format(abbrev)
     return get_or_download_path(year, url, filename, force)
